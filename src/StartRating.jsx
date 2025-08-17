596486aa29240caa8c6bbe18c09293ccd2656b35
Juan Sebastian Pabon Mojica <nova159712@gmail.com>
2025-08-16 20:17:06 -0500

    Añade un componente reutilizable StarRating

--- src/StartRating.jsx
+++ src/StartRating.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
 const containerStyles = {
   display: "flex",
   alignItems: "center",
@@ -6,17 +9,121 @@ const containerStyles = {
 
 const starContainerStars = {
   display: "flex",
-  gap: "4px",
 };
-export default function StarRating({ maxRating = 5 }) {
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.arrayOf(PropTypes.string),
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
+};
+
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
   return (
     <div style={containerStyles}>
       <div style={starContainerStars}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>S{i + 1}</span>
+          <span>
+            <Star
+              key={i}
+              onRate={() => handleRating(i + 1)}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
+              color={color}
+              size={size}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            />
+          </span>
         ))}
       </div>
-      <p>10</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
+
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
+  return (
+    <span
+      role='button'
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+      style={starStyle}>
+      {full ? (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill={color}
+          viewBox='0 0 24 24'
+          stroke={color}>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='{2}'
+            d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z'
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke={color}>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='{2}'
+            d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z'
+          />
+        </svg>
+      )}
+    </span>
+  );
+}
+/* EMPTY STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#000"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  />
+</svg> */
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-/* import './index.css'
-import App from './App.jsx' */
+import "./index.css";
+import App from "./App.jsx";
+import Appv1 from "./App-v1.jsx";
 import StarRating from "./StartRating.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
+    <Appv1 />
 
-    <StarRating maxRating={10} />
-    <StarRating maxRating={5} />
+    {/*   <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
+    />
+    <StarRating
+      color='#a0caecff'
+      size={25}
+      messages={["Bad", "Average", "Good"]}
+      maxRating={3}
+      defaultRating={1}
+    /> */}
   </StrictMode>
 );
